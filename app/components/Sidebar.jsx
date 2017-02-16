c76746f2ef03d7a820dd28d61f8c61da952d0a21
Mark Hario <hario.mark@gmail.com>
2017-02-15 21:37:35 -0500

    ui is responsive

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,48 +1,46 @@
 import React from 'react';
 
 export const Sidebar = props => (
-  <div className="side-bar form-group">
+  <div>
     {
       (props.drawnBoard == false) 
       ?
-      <div className="col-md-3">
-        <form onSubmit={props.handleSubmit}>
-          <h2>Game of Life</h2>
-          <p> Seed life percentage: </p><input name="lifePercent" value={props.lifePercent} onChange={props.handleLifeRatio}/><br />
-          <p> Board Size: </p><input name="boardSize" value={props.boardSize} onChange={props.handleBoardSize}/><br />
-          <hr />
-          <h4>Living Neighbor Rules</h4>
-          <div className="col-xs-3"><strong> Minimum: </strong></div>
-          <div className="col-xs-9"><button className="btn bracket-button" onClick={props.changeNums} name="minLess">&lt;</button><p>{props.minLive}</p>
-            <button className="btn bracket-button" onClick={props.changeNums} name="minMore">&gt;</button><br /></div>
-          <div className="col-xs-3"><strong> Maximum: </strong></div>
-          <div className="col-xs-9"><button className="btn bracket-button" onClick={props.changeNums} name="maxLess">&lt;</button><p>{props.maxLive}</p>
-            <button className="btn bracket-button" onClick={props.changeNums} name="maxMore">&gt;</button><br /></div>
-          <div className="col-xs-3"><strong> Birth: </strong></div>
-          <div className="col-xs-9"><button className="btn bracket-button" onClick={props.changeNums} name="birthLess">&lt;</button><p>{props.numberBirth}</p>
-            <button className="btn bracket-button" onClick={props.changeNums} name="birthMore">&gt;</button><br /></div>
-          <button type="submit" className="btn btn-primary">Build it!</button>
-        </form>
+      <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-12 menu main-menu">
+          <form onSubmit={props.handleSubmit}>
+            <h2>Game of Life</h2>
+            <p> Seed life percentage: </p><input name="lifePercent" value={props.lifePercent} onChange={props.handleLifeRatio}/><br />
+            <p> Board Size: </p><input name="boardSize" value={props.boardSize} onChange={props.handleBoardSize}/><br />
+            <hr />
+            <h4>Living Neighbor Rules</h4>
+            <div className="col-xs-3"><strong> Minimum: </strong></div>
+            <div className="col-xs-9"><button className="btn bracket-button" onClick={props.changeNums} name="minLess">&lt;</button><p>{props.minLive}</p>
+              <button className="btn bracket-button" onClick={props.changeNums} name="minMore">&gt;</button><br /></div>
+            <div className="col-xs-3"><strong> Maximum: </strong></div>
+            <div className="col-xs-9"><button className="btn bracket-button" onClick={props.changeNums} name="maxLess">&lt;</button><p>{props.maxLive}</p>
+              <button className="btn bracket-button" onClick={props.changeNums} name="maxMore">&gt;</button><br /></div>
+            <div className="col-xs-3"><strong> Birth: </strong></div>
+            <div className="col-xs-9"><button className="btn bracket-button" onClick={props.changeNums} name="birthLess">&lt;</button><p>{props.numberBirth}</p>
+              <button className="btn bracket-button" onClick={props.changeNums} name="birthMore">&gt;</button><br /></div>
+            <button type="submit" className="btn btn-primary">Build it!</button>
+          </form>
       </div>
       :
-      <div className="col-xs-12">
-        <div className="row">
-          <div className="col-xs-4">
+      <div className="col-xs-12 menu">
+        <div className="col-xs-4 text-center">
+        <button 
+          className="btn-primary btn"
+          onClick={() => props.cycleTurn()}>Cycle Turn</button>
+        </div>  
+        <div className="col-xs-4 text-center">
           <button 
             className="btn-primary btn"
-            onClick={() => props.cycleTurn()}>Cycle Turn</button>
-          </div>  
-          <div className="col-xs-4">
-            <button 
-              className="btn-primary btn"
-              onClick={() => props.runTurns()}>Play
-            </button>
-          </div>
-          <div className="col-xs-4">
-            <form>
-              <button type="submit" className="btn">Reset</button>
-            </form>
-          </div>
+            onClick={() => props.runTurns()}>Play
+          </button>
+        </div>
+        <div className="col-xs-4 text-center">
+          <form>
+            <button type="submit" className="btn">Reset</button>
+          </form>
         </div>
       </div>  
     }
